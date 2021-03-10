983ce14354cc8eb139fc2291f3ab96472bab559a
ChandanNB01 <80404730+ChandanNB01@users.noreply.github.com>
2021-03-10 20:00:38 +0530

    Add files via upload

---
+++ distancebetweentwopoints.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int x1,x2,y1,y2;
+    float distance=0.0;
+    printf("enter x and y coordinates of first and second point ");
+    scanf("%d %d %d %d",&x1,&x2,&y1,&y2);
+    distance=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+    printf("\n distance = %f",distance);
+    return 0;
+
+}
